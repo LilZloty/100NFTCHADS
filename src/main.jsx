58dfae53df7b69da0b489826ec007b7cd0c4ac28
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2021-11-30 23:36:10 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import '/src/styles/index.css'
 import App from '/src/App.js'
-import { useMediaQuery } from 'react-responsive'
 
 ReactDOM.render(
   <React.StrictMode>
