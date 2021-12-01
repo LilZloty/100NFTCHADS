ee8d8ed5be0ad7df3019c5eb3335d27f3b2e09ce
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2021-11-30 23:43:50 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import '/src/styles/index.css'
-import App from '/src/App.jsx'
+import './styles/index.css'
+import App from './App.jsx'
 
 ReactDOM.render(
   <React.StrictMode>
