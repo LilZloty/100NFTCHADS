6b7858688172ab661e1204b10fff73e6aef460b4
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2021-11-30 23:35:25 -0600

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
-import App from './App'
+import '/src/styles/index.css'
+import App from '/src/App.js'
 import { useMediaQuery } from 'react-responsive'
 
 ReactDOM.render(
